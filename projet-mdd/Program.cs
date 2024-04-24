62864e2ab33d8859e8dfec0555b2a9d5efd9760e
sophieeanne <annesophieassiene@gmail.com>
2024-04-24 12:22:36 +0200

    modif des commandes (je vais les faire sur mysql bien et les coller ici)

--- projet-mdd/Program.cs
+++ projet-mdd/Program.cs
@@ -24,25 +24,25 @@ class Program
 
     static void Creation_Tables(MySqlConnection maConnexion)
     {
-        string createTable = "CREATE TABLE IF NOT EXISTS modèle (nprod INT, nom VARCHAR(255), grandeur VARCHAR(255), prix_u INT, ligne_produit VARCHAR(255), date_intro DATE, date_disco DATE, PRIMARY KEY (nprod));";
+        string createTable = "";
         MySqlCommand maCommande = maConnexion.CreateCommand();
         maCommande.CommandText = createTable;
-        string createTable_pièce = "CREATE TABLE IF NOT EXISTS pièce (nprod_p INT, description VARCHAR(255), nom_f VARCHAR(255), nprod_f INT, prix_u_p INT, date_commande DATE, adresse VARCHAR(255), date_livraison DATE, quantité INT, PRIMARY KEY (nprod_p));";
+        string createTable_pièce = "";
         MySqlCommand maCommande2 = maConnexion.CreateCommand();
         maCommande2.CommandText = createTable_pièce;
-        string createTable_fournisseur = "CREATE TABLE IF NOT EXISTS fournisseur (siret INT, contact VARCHAR(255), adresse VARCHAR(255), libellé INT, PRIMARY KEY (siret));";
+        string createTable_fournisseur = "";
         MySqlCommand maCommande3 = maConnexion.CreateCommand();
         maCommande3.CommandText = createTable_fournisseur;
-        string createTable_client = "CREATE TABLE IF NOT EXISTS client (nclient INT, nom VARCHAR(255), prenom VARCHAR(255), adresse VARCHAR(255), telephone INT, courriel VARCHAR(255), PRIMARY KEY (nclient));";
+        string createTable_client = "";
         MySqlCommand maCommande4 = maConnexion.CreateCommand();
         maCommande4.CommandText = createTable_client;
-        string createTable_boutique = "CREATE TABLE IF NOT EXISTS boutique (nom_compagnie VARCHAR(255), adresse VARCHAR(255), telephone INT, courriel VARCHAR(255), nom_b VARCHAR(255), PRIMARY KEY (nom_compagnie))";
+        string createTable_boutique = "";
         MySqlCommand maCommande5 = maConnexion.CreateCommand();
         maCommande5.CommandText = createTable_boutique;
-        string createTable_Fidélio = "CREATE TABLE IF NOT EXISTS fidélio (nprogramme INT, description VARCHAR(255), cout INT, durée VARCHAR(255), rabais INT, PRIMARY KEY(nprogramme));";
+        string createTable_Fidélio = ";";
         MySqlCommand maCommande6 = maConnexion.CreateCommand();
         maCommande6.CommandText = createTable_Fidélio;
-        string createTable_assemblage = "CREATE TABLE IF NOT EXISTS assemblage (nom VARCHAR(255), FOREIGN KEY REFERENCES modèle(nom), ;"; 
+
 
         try
         {
